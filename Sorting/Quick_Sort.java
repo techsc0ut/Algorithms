e0daea775b9ebec9d1072ac85edf037fb0471215
techsc0ut <techsc0utOP@gmail.com>
2021-04-26 20:04:27 +0530

    Update Quick_Sort.java

--- Sorting/Quick_Sort.java
+++ Sorting/Quick_Sort.java
@@ -1,35 +1,34 @@
 public class Quick_Sort{	
 	public static void main(String[] args){
 		int ar[]= {111111,-9,-1299,-8,-3,-8,-4};
-		quicksort(ar,0,ar.length);
+		quicksort(ar,0,ar.length-1);
 		for(int h:ar){
 			System.out.print(h+" ");
 		}
 	}
 	public static void quicksort(int ar[],int start,int end){
-		if(end-start<2){
-			return;
+		if(start<end){
+			int part=partition(ar,start,end);
+			quicksort(ar,start,part-1);
+			quicksort(ar,part+1,end);
 		}
-		int pivotindex=partition(ar,start,end);
-		quicksort(ar,start,pivotindex);
-		quicksort(ar,pivotindex+1,end);
 	}
 	public static int partition(int [] ar,int start,int end){	
-		int i=start;
-		int j=end;
-		int pivot=ar[i];
-		while(i<j){
-			while(i<j && ar[--j]>=pivot);
-			if(i<j){
+		int pivot=ar[end];
+		int i=start-1;
+		int temp;
+		for(int j=start;j<=end-1;j++){
+			if(pivot>=ar[j]){
+				i+=1;
+				temp=ar[i];
 				ar[i]=ar[j];
-			}
-			while(i<j && ar[++i]<=pivot);
-			if(i<j){
-				ar[j]=ar[i];
+				ar[j]=temp;
 			}
 		}
-		ar[j]=pivot;
-		return j;
+		temp=ar[i+1];
+		ar[i+1]=ar[end];
+		ar[end]=temp;
+		return i+1;
 	}
 }
 
