17a6957cb75237b120b903f08890eb9ddc12bf4d
techsc0ut <techsc0utOP@gmail.com>
2021-05-09 13:58:30 +0530

    Update Print_All_Subsequence.java

--- Strings/Print_All_Subsequence.java
+++ Strings/Print_All_Subsequence.java
@@ -1,19 +1,20 @@
+/*package whatever //do not write package name here */
+
 import java.io.*;
 import java.util.*;
 
-class GFG {
-    static ArrayList<String> A=new ArrayList<>();
-	public static void main (String[] args) {
-		String S="aabc", V="";
-		func(S,V);
+class practice{	static ArrayList<String> A=new ArrayList();
+		public static void main (String[] args) {
+		String S="aabc";
+		func(S,"");
 		System.out.println(A);
 	}
 	public static void func(String input, String Output){
-	    if(input.length()==0){
-	        A.add(Output);
-	        return;
-	    }
-	    func(input.substring(1),Output+input.charAt(0));
-	    func(input.substring(1),Output);
-	}
+	   	if(input.length()==0){
+			A.add(Output);
+			return;
+		}
+		func(input.substring(1),Output);
+		func(input.substring(1),Output+input.charAt(0));
+	}	
 }
