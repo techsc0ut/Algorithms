44d0582558fb4a5a41ab418f991e862c7070c857
techsc0ut <techsc0utOP@gmail.com>
2021-05-27 17:46:01 +0530

    Update Merge_Two_Sorted_Arrays_O(1)_Space.java

--- Arrays/Merge_Two_Sorted_Arrays_O(1)_Space.java
+++ Arrays/Merge_Two_Sorted_Arrays_O(1)_Space.java
@@ -1,50 +1,32 @@
-/*package whatever //do not write package name here */
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    static int nextGap(int gap)
-    {
+class Solution{
+    static int nextGap(int gap){
         if (gap <= 1)
             return 0;
         return (gap / 2) + (gap % 2);
     }
-	public static void main (String[] args) {
-	    int ar1[]={1,81,100,122};
-	    int ar2[]={78,90,91};
-	    Merge(ar1,ar2);
-	    for(int x:ar1){
-	        System.out.print(x+" ");
-	    }
-	    System.out.println();
-	    for(int y:ar2){
-	        System.out.print(y+" ");
-	    }
-	}
-	static void Merge(int ar1[],int ar2[]){
-	    int org=ar1.length+ar2.length;
+    public static void merge(long ar1[], long ar2[], int n, int m) {
+        int org=n+m;
 	    int gap=nextGap(org);
 	    while(gap!=0){
 	        int i=0,j=gap;
 	        while(i<org && j<org){
-	            if(i<ar1.length && j<ar1.length){
+	            if(i<n && j<n){
 	                if(ar1[i]>ar1[j]){
 	                    swap(ar1,i,ar1,j);
 	                    i++;j++;
 	                }else{
 	                    i++;j++;
 	                }
-	            }else if(i<ar1.length && j>=ar1.length && j<org){
-	                int ind=j-ar1.length;
+	            }else if(i<n && j>=n && j<org){
+	                int ind=j-n;
 	                if(ar1[i]>ar2[ind]){
 	                    swap(ar1,i,ar2,ind);
 	                    i++;j++;
 	                }else{
 	                    i++;j++;
 	                }
-	            }else if(i>=ar1.length && j>=ar1.length && i<org && j<org){
-	                int ind1=i-ar1.length,ind2=j-ar1.length;
+	            }else if(i>=n && j>=n && i<org && j<org){
+	                int ind1=i-n,ind2=j-n;
 	                if(ar2[ind1]>ar2[ind2]){
 	                    swap(ar2,ind1,ar2,ind2);
 	                    i++;
@@ -57,10 +39,10 @@ class GFG {
 	            }
 	        }
 	        gap=nextGap(gap);
-	    }
-	}
-	static void swap(int ar1[],int a,int ar2[],int b){
-	    int temp=ar1[a];
+	    }    
+    }
+    static void swap(long ar1[],int a,long ar2[],int b){
+	    long temp=ar1[a];
 	    ar1[a]=ar2[b];
 	    ar2[b]=temp;
 	}
