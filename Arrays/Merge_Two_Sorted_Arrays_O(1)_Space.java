93aff3df82817c45237e59b611991782111a3264
techsc0ut <techsc0utOP@gmail.com>
2021-05-27 17:38:37 +0530

    Update Merge_Two_Sorted_Arrays_O(1)_Space.java

--- Arrays/Merge_Two_Sorted_Arrays_O(1)_Space.java
+++ Arrays/Merge_Two_Sorted_Arrays_O(1)_Space.java
@@ -1,47 +1,67 @@
-class Solution {
-    public void merge(int arr1[], int arr2[], int n, int m) {
-        int i=0,j=0,k=n-1;
-        while(i<=k && j<m){
-            if(arr1[i]<arr2[j]){
-                i++;
-            }else{
-                int temp=arr1[k];
-                arr1[k]=arr2[j];
-                arr2[j]=temp;
-                k--;
-                j++;
-            }
-        }
-        mergeSort(arr1,0,arr1.length-1);
-        mergeSort(arr2,0,arr2.length-1);
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    static int nextGap(int gap)
+    {
+        if (gap <= 1)
+            return 0;
+        return (gap / 2) + (gap % 2);
     }
-    public void mergeSort(int ar[],int start,int end){
-        if(start<end){
-            int mid=(start+end)/2;
-            mergeSort(ar,start,mid);
-            mergeSort(ar,mid+1,end);
-            merge(ar,start,mid,end);
-        }
-    }
-    public void merge(int ar[],int start,int mid,int end){
-        int left[]=Arrays.copyOfRange(ar,start,mid+1);
-        int right[]=Arrays.copyOfRange(ar,mid+1,end+1);
-        int i=0,j=0;
-        int index=start;
-        int n=left.length;
-        int m=right.length;
-        while(i<n && j<m){
-            if(left[i]>right[j]){
-                ar[index++]=right[j++];
-            }else{
-                ar[index++]=left[i++];
-            }
-        }
-        while(i<n){
-            ar[index++]=left[i++];
-        }
-        while(j<m){
-            ar[index++]=right[j++];
-        }
-    }
+	public static void main (String[] args) {
+	    int ar1[]={1,81,100,122};
+	    int ar2[]={78,90,91};
+	    Merge(ar1,ar2);
+	    for(int x:ar1){
+	        System.out.print(x+" ");
+	    }
+	    System.out.println();
+	    for(int y:ar2){
+	        System.out.print(y+" ");
+	    }
+	}
+	static void Merge(int ar1[],int ar2[]){
+	    int org=ar1.length+ar2.length;
+	    int gap=nextGap(org);
+	    while(gap!=0){
+	        int i=0,j=gap;
+	        while(i<org && j<org){
+	            if(i<ar1.length && j<ar1.length){
+	                if(ar1[i]>ar1[j]){
+	                    swap(ar1,i,ar1,j);
+	                    i++;j++;
+	                }else{
+	                    i++;j++;
+	                }
+	            }else if(i<ar1.length && j>=ar1.length && j<org){
+	                int ind=j-ar1.length;
+	                if(ar1[i]>ar2[ind]){
+	                    swap(ar1,i,ar2,ind);
+	                    i++;j++;
+	                }else{
+	                    i++;j++;
+	                }
+	            }else if(i>=ar1.length && j>=ar1.length && i<org && j<org){
+	                int ind1=i-ar1.length,ind2=j-ar1.length;
+	                if(ar2[ind1]>ar2[ind2]){
+	                    swap(ar2,ind1,ar2,ind2);
+	                    i++;
+	                    j++;
+	                }else{
+	                    i++;j++;
+	                }
+	            }else{
+	                break;
+	            }
+	        }
+	        gap=nextGap(gap);
+	    }
+	}
+	static void swap(int ar1[],int a,int ar2[],int b){
+	    int temp=ar1[a];
+	    ar1[a]=ar2[b];
+	    ar2[b]=temp;
+	}
 }
