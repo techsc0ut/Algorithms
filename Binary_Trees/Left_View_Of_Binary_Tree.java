f45ec87200b17aaea8007f6a8c7ec92777501fae
TECH SCOUT <techsc0utOP@gmail.com>
2021-06-09 01:13:00 +0530

    Create Left_View_Of_Binary_Tree.java

---
+++ Binary_Trees/Left_View_Of_Binary_Tree.java
@@ -0,0 +1,27 @@
+class Tree{
+    ArrayList<Integer> leftView(Node root){
+        ArrayList<Integer> A=new ArrayList<>();
+        Queue<Node> Q=new LinkedList();
+        func(A,Q,root);
+        return A;
+    }
+    static void func(ArrayList<Integer> A,Queue<Node> Q,Node root){
+        Q.add(root);
+        while(!Q.isEmpty()){
+            int n=Q.size();
+            for(int i=1;i<=n;i++){
+                Node a=Q.remove();
+                if(i==1 && a!=null){
+                    A.add(a.data);
+                }
+                if(a!=null && a.left!=null){
+                    Q.add(a.left);
+                }
+                if(a!=null && a.right!=null){
+                    Q.add(a.right);
+                }
+            }
+        }
+        return ;
+    }
+}
