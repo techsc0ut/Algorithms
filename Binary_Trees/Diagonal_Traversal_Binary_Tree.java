acfdf860cf8c5516a8260be02da51c8c65e7c73e
TECH SCOUT <techsc0utOP@gmail.com>
2021-06-09 14:15:46 +0530

    More Efficient solution

--- Binary_Trees/Diagonal_Traversal_Binary_Tree.java
+++ Binary_Trees/Diagonal_Traversal_Binary_Tree.java
@@ -1,36 +1,18 @@
 class Tree{
     public ArrayList<Integer> diagonal(Node root){
         ArrayList<Integer> A=new ArrayList<>();
-        TreeMap<Integer,ArrayList<Integer>> T=new TreeMap<>();
-        func(root,T,0,0);
-        for(ArrayList<Integer> At:T.values()){
-            for(int p:At){
-                A.add(p);
+        Queue<Node> Q=new LinkedList<>();
+        Q.add(root);
+        while(!Q.isEmpty()){
+            Node curr=Q.remove();
+            while(curr!=null){
+                A.add(curr.data);
+                if(curr.left!=null){
+                    Q.add(curr.left);
+                }
+                curr=curr.right;
             }
-        }return A;
-    }
-    static void func(Node root ,TreeMap<Integer,ArrayList<Integer>> T,int h,int l){
-        if(root==null){
-            return ;
         }
-        ArrayList<Integer> A=T.get(h-l);
-        if(A==null){
-            A=new ArrayList<>();
-            T.put(h-l,A);
-        }
-        A.add(root.data);
-        func(root.left,T,h+1,l-1);
-        func(root.right,T,h+1,l+1);
-    }
-}
-class temp{
-    Node n;
-    int h;
-    int l;
-    temp(){}
-    temp(Node nn,int hh,int ll){
-        this.n=nn;
-        this.h=hh;
-        this.l=ll;
+        return A;
     }
 }
