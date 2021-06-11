cafaf54e7c69b6846c3b2d430db850528e98ad80
TECH SCOUT <techsc0utOP@gmail.com>
2021-06-11 20:12:32 +0530

    Create Delete_node_from_BST.java

---
+++ Binary_Search_Tree/Delete_node_from_BST.java
@@ -0,0 +1,30 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        if(root.val>key){
+            root.left=deleteNode(root.left,key);
+        }else if(root.val<key){
+            root.right=deleteNode(root.right,key);
+        }else{
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            TreeNode temp=inSuc(root.right);
+            root.val=temp.val;
+            root.right=deleteNode(root.right,temp.val);
+        }
+        return root;        
+    }
+    static TreeNode inSuc(TreeNode root){
+        TreeNode curr=root;
+        while(curr!=null && curr.left!=null){
+            curr=curr.left;
+        }
+        return curr;
+    }
+}
