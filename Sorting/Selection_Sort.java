b8d1746e9dd8c298419ea1d8cb4b21bc14849df2
TECH SCOUT <techsc0utOP@gmail.com>
2021-07-02 16:26:56 +0530

    Create Selection_Sort.java

---
+++ Sorting/Selection_Sort.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int ar[]={23,211234,-100,321,555};
+		Selection_Sort(ar,ar.length);
+		for(int x:ar){
+		    System.out.print(x+" ");
+		}
+	}
+	static void Selection_Sort(int ar[],int n){
+	    for(int i=0;i<n;i++){
+	        int index=i;
+	        for(int j=i+1;j<n;j++){
+	            if(ar[index]>ar[j]){
+	                index=j;
+	            }
+	        }
+	        int temp=ar[index];
+	        ar[index]=ar[i];
+	        ar[i]=temp;
+	    }
+	}
+}
