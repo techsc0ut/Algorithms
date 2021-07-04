adffbf7cd7991add22227544d9abc00fcb44346b
TECH SCOUT <techsc0utOP@gmail.com>
2021-07-04 16:43:45 +0530

    Update Merge_Two_Sorted_Arrays_O(1)_Space.java

--- Arrays/Merge_Two_Sorted_Arrays_O(1)_Space.java
+++ Arrays/Merge_Two_Sorted_Arrays_O(1)_Space.java
@@ -1,49 +1,41 @@
-class Solution{
-    static int nextGap(int gap){
-        if (gap <= 1)
+class Solution {
+    static int getGAP(int x){
+        if(x<=1){
             return 0;
-        return (gap / 2) + (gap % 2);
+        }
+        return x/2+x%2;
     }
-    public static void merge(long ar1[], long ar2[], int n, int m) {
-        int org=n+m;
-	    int gap=nextGap(org);
-	    while(gap!=0){
-	        int i=0,j=gap;
-	        while(i<org && j<org){
-	            if(i<n && j<n){
-	                if(ar1[i]>ar1[j]){
-	                    swap(ar1,i,ar1,j);
-	                    i++;j++;
-	                }else{
-	                    i++;j++;
-	                }
-	            }else if(i<n && j>=n && j<org){
-	                int ind=j-n;
-	                if(ar1[i]>ar2[ind]){
-	                    swap(ar1,i,ar2,ind);
-	                    i++;j++;
-	                }else{
-	                    i++;j++;
-	                }
-	            }else if(i>=n && j>=n && i<org && j<org){
-	                int ind1=i-n,ind2=j-n;
-	                if(ar2[ind1]>ar2[ind2]){
-	                    swap(ar2,ind1,ar2,ind2);
-	                    i++;
-	                    j++;
-	                }else{
-	                    i++;j++;
-	                }
-	            }else{
-	                break;
-	            }
-	        }
-	        gap=nextGap(gap);
-	    }    
+    public void merge(int arr1[], int arr2[], int n, int m) {
+        int t=n+m;
+        int gap=getGAP(n+m);
+        while(gap!=0){
+            int i=0,j=gap;
+            while(i<t && j<t){
+                if(i<n && j<n){
+                    if(arr1[i]>arr1[j]){
+                        swap(arr1,i,arr1,j);
+                    }       
+                }
+                else if(i<n && j>=n && j<t){
+                    int J=j-n;
+                    if(arr1[i]>arr2[J]){
+                        swap(arr1,i,arr2,J);
+                    }
+                }
+                else if(i>=n && j>=n && i<t && j<t){
+                    int I=i-n,J=j-n;
+                    if(arr2[I]>arr2[J]){
+                        swap(arr2,I,arr2,J);
+                    }
+                }
+                i++;j++;
+            }
+            gap=getGAP(gap);
+        }
     }
-    static void swap(long ar1[],int a,long ar2[],int b){
-	    long temp=ar1[a];
-	    ar1[a]=ar2[b];
-	    ar2[b]=temp;
-	}
+    static void swap(int ar1[],int a,int ar2[],int b){
+        int temp=ar1[a];
+        ar1[a]=ar2[b];
+        ar2[b]=temp;
+    }
 }
