39ab3ec5dcfe8fcc3449a582c6079c6ba7efc8e7
TECH SCOUT <techsc0utOP@gmail.com>
2021-10-02 13:52:55 +0530

    Update Next_permutation.java

--- Arrays/Next_permutation.java
+++ Arrays/Next_permutation.java
@@ -1,35 +1,34 @@
 // Given an array , rearrange to give the next permutation using the elements , if not then just sort them .
 class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums.length==1){
-            return ;
-        }
-        for(int i=nums.length-1;i>=1;i--){
-            if(nums[i-1]<nums[i]){
-                int a=nums[i-1];
-                for(int j=nums.length-1;j>=i;j--){
-                    if(nums[j]>a){
-                        swap(nums,j,i-1);
-                        break;
-                    }
-                }
-                int as=i,b=nums.length-1;
-                while(as<b){
-                    int temp=nums[as];
-                    nums[as]=nums[b];
-                    nums[b]=temp;
-                    as++;
-                    b--;
-                }
-                return;   
+        int i=0, ind=-1;
+        for(i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
+                break;
             }
         }
-        Arrays.sort(nums);
-        return;
+        if(ind==-1){
+            Arrays.sort(nums);
+        }else{
+            for(int j=nums.length-1;j>=0;j--){
+                if(nums[ind]<nums[j]){
+                    swap(nums,ind,j);
+                    break;
+                }
+            }
+            int a=ind+1;
+            int b=nums.length-1;
+            while(a<b){
+                swap(nums,a,b);
+                a++;
+                b--;
+            }  
+        }
     }
-    public void swap(int[] nums,int a,int b){
-        int temp=nums[a];
-        nums[a]=nums[b];
-        nums[b]=temp;
+    static void swap(int nums[],int i,int j){
+        int tmp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=tmp;
     }
 }
