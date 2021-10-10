81871c7966e7f3910637cd59133a3e6ec7f66be7
TECH SCOUT <techsc0utOP@gmail.com>
2021-10-10 18:04:59 +0530

    Update Fractional_Knapsack.java

--- Greedy/Fractional_Knapsack.java
+++ Greedy/Fractional_Knapsack.java
@@ -1,14 +1,14 @@
 class Solution{
-    double fractionalKnapsack(int W, Item arr[], int n) {
+    double fractionalKnapsack(int W, Item arr[], int n){
         Arrays.sort(arr,new comp());
         double profit=0;
-        int w=0;
+        int curW=0;
         for(int i=0;i<n;i++){
-            if(w+arr[i].weight<=W){
-                w+=arr[i].weight;
+            if(curW+arr[i].weight<=W){
+                curW=curW+arr[i].weight;
                 profit+=arr[i].value;
             }else{
-                int rem=W-w;
+                int rem=W-curW;
                 profit+=arr[i].value*((double)rem/arr[i].weight)*1.0;
                 break;
             }
@@ -18,13 +18,12 @@ class Solution{
 }
 class comp implements Comparator<Item>{
     public int compare(Item a, Item b){
-        double r1 = (double)a.value / a.weight;
-        double r2 = (double)b.value / b.weight;
-        if(r1 > r2){
-            return -1;
-        } else if(r1 < r2) {
+        double aa=(double)a.value/a.weight;
+        double bb=(double)b.value/b.weight;
+        if(bb>aa){
             return 1;
-        }
-        return 0;
+        }else if(bb<aa){
+            return -1;
+        }return 0;
     }
 }
