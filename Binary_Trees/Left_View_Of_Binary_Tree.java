ecb5285df6834dcf67c344e845744d9131ef0f2f
TECH SCOUT <techsc0utOP@gmail.com>
2021-10-27 22:28:34 +0530

    Update Left_View_Of_Binary_Tree.java

--- Binary_Trees/Left_View_Of_Binary_Tree.java
+++ Binary_Trees/Left_View_Of_Binary_Tree.java
@@ -1,28 +1,36 @@
-// Approach is , we need 1st element of every Level .
 class Tree{
     ArrayList<Integer> leftView(Node root){
-        ArrayList<Integer> A=new ArrayList<>();
-        Queue<Node> Q=new LinkedList();
-        func(A,Q,root);
-        return A;
-    }
-    static void func(ArrayList<Integer> A,Queue<Node> Q,Node root){
-        Q.add(root);
+        TreeMap<Integer,Integer> T=new TreeMap<>();
+        Queue<temp> Q=new LinkedList<>();
+        Q.add(new temp(root,0));
         while(!Q.isEmpty()){
-            int n=Q.size();
-            for(int i=1;i<=n;i++){
-                Node a=Q.remove();
-                if(i==1 && a!=null){
-                    A.add(a.data);
-                }
-                if(a!=null && a.left!=null){
-                    Q.add(a.left);
-                }
-                if(a!=null && a.right!=null){
-                    Q.add(a.right);
-                }
+            temp t=Q.poll();
+            Node n=t.n;
+            int h=t.h;
+            Integer a=T.get(h);
+            if(n!=null && a==null){
+                a=n.data;
+                T.put(h,a);
+            }
+            if(n!=null && n.left!=null){
+                Q.offer(new temp(n.left,h+1));
+            }
+            if(n!=null && n.right!=null){
+                Q.offer(new temp(n.right,h+1));
             }
         }
-        return ;
+        ArrayList<Integer> Res=new ArrayList<>();
+        for(Integer x:T.values()){
+            Res.add(x);
+        }
+        return Res;
+    }
+}
+class temp{
+    Node n;
+    int h;
+    temp(Node _n,int _h){
+        this.n=_n;
+        this.h=_h;
     }
 }
