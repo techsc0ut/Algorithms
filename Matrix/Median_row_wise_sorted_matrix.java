5e400a3229ee39503b0185181412baa9b319dece
TECH SCOUT <techsc0utOP@gmail.com>
2021-11-26 21:13:04 +0530

    efficient

--- Matrix/Median_row_wise_sorted_matrix.java
+++ Matrix/Median_row_wise_sorted_matrix.java
@@ -1,37 +1,38 @@
 class Solution {
-    int median(int m[][],int r,int c){
-    int max=Integer.MIN_VALUE;
-    int min=Integer.MAX_VALUE;
-    for(int i=0;i<r;i++){
-      if(m[i][0]<min){
-        min=m[i][0]; 
-      }
-      if(m[i][c-1]>max){
-        max=m[i][c-1];
-      }
-    }
-    int desired=(1+r*c)/2;
-    while(min<max){
-      int mid=(min+max)/2;
-      int places=0;
-      int get=0;
-      for(int i=0;i<r;i++){
-        get=Arrays.binarySearch(m[i],mid);
-        if(get<0){
-          get=Math.abs(get)-1;
-        }else{
-          while(get<m[0].length && m[i][get]==mid){
-            get+=1;
-          }
+    int median(int matrix[][], int r, int c) {
+        int max=2000;
+        int min=1;
+        int desired=(1+r*c)/2;
+        while(min<max){
+            int mid=(min+max)/2;
+            int places=0;
+            int get=0;
+            for(int i=0;i<r;i++){
+                get=func(mid,i,matrix);
+                if(get<0){
+                    get=Math.abs(get)-1;
+                }
+                places+=get;
+            }
+            if(places<desired){
+                min=mid+1; 
+            }else{
+                max=mid;
+            }
         }
-        places+=get;
-      }
-      if(places<desired){
-        min=mid+1; 
-      }else{
-        max=mid;
-      }
+        return min;
     }
-    return min;
-  }
+    static int func(int x, int r, int matrix[][]){
+        int start=0;
+        int end=matrix[0].length;
+        while(start<end){
+            int mid=(start+end)/2;
+            if(matrix[r][mid]<=x){
+                start=mid+1;
+            }else{
+                end=mid;
+            }
+        }
+        return start;
+    }
 }
