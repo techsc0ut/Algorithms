ba87b57413e92061f5ccfd5c94892d6d93104046
TECH SCOUT <techsc0utOP@gmail.com>
2022-01-11 20:28:44 +0530

    Update Insertion_Sort.java

--- Sorting/Insertion_Sort.java
+++ Sorting/Insertion_Sort.java
@@ -1,15 +1,20 @@
 /* Best Time Complexity is O(n) , occurs when array is pre-sorted 
    whereas the Average and Worst case time complexity is O(n^2) */
-
-void Insertion_sort(int arr[]){
-		int n=arr.length;
-      	for(int i=0;i<n;i++){
-        	int temp=arr[i];
-          	int j=i-1;
-          	while(j>=0 && arr[j]>temp){
-              	arr[j+1]=arr[j];
-              	j--;
+class Solution{
+    public void insertionSort(int arr[], int n){
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]>arr[j+1]){
+                    swap(arr,j,j+1);
+                }else{
+                    break;
+                }
             }
-          	arr[j+1]=temp;
         }
-	}
+    }
+    static void swap(int arr[],int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
