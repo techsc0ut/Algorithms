bc2408b9637d677bb390ef00a8a3c6e90cabc2ec
TECH SCOUT <techsc0utOP@gmail.com>
2022-02-02 15:25:03 +0530

    Update Diameter_of_binary_Tree(O(n)).java

--- Binary_Trees/Diameter_of_binary_Tree(O(n)).java
+++ Binary_Trees/Diameter_of_binary_Tree(O(n)).java
@@ -1,23 +1,16 @@
 class Solution {
-    int diameter(Node root) {
+    int max=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        func(root);
+        return max;
+    }
+    int func(TreeNode root){
         if(root==null){
             return 0;
         }
-        A a=new A();
-        height(root,a);
-        return a.ans;
-       
+        int left=func(root.left);
+        int right=func(root.right);
+        max=Math.max(max,left+right);
+        return left>right?left+1:right+1;
     }
-    int height(Node root, A a){
-        if(root==null){
-            return 0;
-        }
-        int left=height(root.left,a);
-        int right=height(root.right,a);
-        a.ans=Math.max(a.ans,left+right+1);
-        return 1+Math.max(left,right);
-    }
-}
-class A{
-    int ans=Integer.MIN_VALUE;
 }
