8e1cd57042f74efbb724aa8788361640d8c94711
TECH SCOUT <techsc0utOP@gmail.com>
2022-03-16 10:15:39 +0530

    Update Validate_Stack_Sequences.java

--- Stack/Validate_Stack_Sequences.java
+++ Stack/Validate_Stack_Sequences.java
@@ -1,3 +1,4 @@
+// O(n) / O(n) Space
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         int j=0;
@@ -10,5 +11,26 @@ class Solution {
             }
         }
         return St.isEmpty();
+    }
+}
+
+
+
+
+
+// O(n) / O(1) Space
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0; // Intialise one pointer pointing on pushed array
+        int j = 0; // Intialise one pointer pointing on popped array
+        
+        for(int val : pushed){
+            pushed[i++] = val; // using pushed as the stack.
+            while(i > 0 && pushed[i - 1] == popped[j]){ // pushed[i - 1] values equal to popped[j];
+                i--; // decrement i
+                j++; // increment j
+            }
+        }
+        return i == 0; // Since pushed is a permutation of popped so at the end we are supposed to be left with an empty stack
     }
 }
