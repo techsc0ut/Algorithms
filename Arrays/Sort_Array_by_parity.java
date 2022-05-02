1e76c2f0b48178f3d72d0b57eb906726c63e572a
TECH SCOUT <techsc0utOP@gmail.com>
2022-05-02 11:21:17 +0530

    Create Sort_Array_by_parity.java

---
+++ Arrays/Sort_Array_by_parity.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int i=0, j=nums.length-1;
+        while(i<j){
+            if(nums[i]%2>nums[j]%2){
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+            }
+            if(nums[i]%2==0){
+                i++;
+            }
+            if(nums[j]%2==1){
+                j--;
+            }
+        }return nums;
+    }
+}
