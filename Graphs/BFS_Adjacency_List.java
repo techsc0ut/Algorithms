409a1b4b5cf72a4652eab87f121d9c9dc8318047
TECH SCOUT <techsc0utOP@gmail.com>
2022-05-16 17:19:41 +0530

    Update BFS_Adjacency_List.java

--- Graphs/BFS_Adjacency_List.java
+++ Graphs/BFS_Adjacency_List.java
@@ -1,23 +1,38 @@
-class Solution{
-    public ArrayList<Integer> bfsOfGraph(int V,ArrayList<ArrayList<Integer>> adj){
-        boolean vis[]=new boolean[V];
-        for(int i=0;i<V;i++){
-            vis[i]=false;
+ArrayList<ArrayList<Integer>> adj = new ArrayList<>(); // adjacency list representation
+
+int n; // number of nodes
+int s; // source vertex
+
+
+LinkedList<Integer> q = new LinkedList<Integer>();
+boolean used[] = new boolean[n];
+int d[] = new int[n];
+int p[] = new int[n];
+
+q.push(s);
+used[s] = true;
+p[s] = -1;
+while (!q.isEmpty()) {
+    int v = q.pop();
+    for (int u : adj.get(v)) {
+        if (!used[u]) {
+            used[u] = true;
+            q.push(u);
+            d[u] = d[v] + 1;
+            p[u] = v;
         }
-        ArrayList<Integer> A=new ArrayList<>();
-        Queue<Integer> Q=new LinkedList();
-        Q.add(0);
-        vis[0]=true;
-        while(!Q.isEmpty()){
-            Integer x=Q.remove();
-            A.add(x);
-            for(int y: adj.get(x)){
-                if(!vis[y]){
-                    Q.add(y);  
-                    vis[y]=true;
-                }
-            }
-        }
-        return A;
     }
+}
+
+
+/* For Path Printing */
+if (!used[u]) {
+    System.out.println("No path!");
+} else {
+    ArrayList<Integer> path = new ArrayList<Integer>();
+    for (int v = u; v != -1; v = p[v])
+        path.add(v);
+    Collections.reverse(path);
+    for(int v : path)
+        System.out.println(v);
 }
