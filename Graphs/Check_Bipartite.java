101cb0d44237b5e0b90c7bec677e9bb08280fa18
TECH SCOUT <techsc0utOP@gmail.com>
2022-05-23 17:58:17 +0530

    Update Check_Bipartite.java

--- Graphs/Check_Bipartite.java
+++ Graphs/Check_Bipartite.java
@@ -1,41 +1,30 @@
 class Solution {
-    int color[];
     public boolean isBipartite(int[][] graph) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         int V=graph.length;
+        int color[]=new int[V];
         for(int i=0;i<V;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<graph.length;i++){
-            for(int x:graph[i]){
-                adj.get(i).add(x);
-            }
-        }
-        color=new int[V];
-        Arrays.fill(color,-1);
-        for(int i=0;i<V;i++){                      // This loop will take care if their are more than one components of graph .
-            if(color[i]==-1){
-                if(!IsBipartite(i,1,adj)){
+            if(color[i]==0){
+                if(!IsBipartite(color,1,i,graph)){
                     return false;
                 }
             }
         }return true;
     }
-    public boolean IsBipartite(int src, int col, ArrayList<ArrayList<Integer>> adj){
+    public boolean IsBipartite(int color[], int col, int src, int[][] graph){
         color[src]=col;
         Queue<Integer> Q=new LinkedList<>();
-        Q.offer(src);
+        Q.add(src);
         while(!Q.isEmpty()){
-            int curr=Q.poll();
-            int co=1-color[curr];
-            for(int x:adj.get(curr)){
-                if(color[x]!=-1){
-                    if(color[curr]==color[x]){
+            int u=Q.poll();
+            int cl=color[u]==1?2:1;
+            for(int v:graph[u]){
+                if(color[v]==0){
+                    color[v]=cl;
+                    Q.add(v);
+                }else if(color[v]!=0){
+                    if(color[v]==color[u]){
                         return false;
                     }
-                }else{
-                    color[x]=co;
-                    Q.offer(x);
                 }
             }
         }return true;
