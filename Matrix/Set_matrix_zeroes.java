80fae16ad53cf1015ca680ee69f370f31ac129e3
TECH SCOUT <techsc0utOP@gmail.com>
2022-06-04 15:41:55 +0530

    Update Set_matrix_zeroes.java

--- Matrix/Set_matrix_zeroes.java
+++ Matrix/Set_matrix_zeroes.java
@@ -1,34 +1,43 @@
+/* Here we are starting our loop from row 1 and col 1 so to avoid corner cases, and hence handling 1st row and 1st column at last */
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        boolean isCol=false;
-        int R=matrix.length;
-        int C=matrix[0].length;
-        for(int i=0;i<R;i++){
-            if(matrix[i][0]==0){
-                isCol=true;
+    public void setZeroes(int[][] mat) {
+        boolean isFirstRow=false;
+        boolean isFirstCol=false;
+        for(int i=0;i<mat[0].length;i++){
+            if(mat[0][i]==0){
+                isFirstRow=true;
+                break;
             }
-            for(int j=1;j<C;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+        }
+        for(int i=0;i<mat.length;i++){
+            if(mat[i][0]==0){
+                isFirstCol=true;
+                break;
+            }
+        }
+        for(int i=1;i<mat.length;i++){
+            for(int j=1;j<mat[0].length;j++){
+                if(mat[i][j]==0){
+                    mat[i][0]=0;
+                    mat[0][j]=0;
                 }
             }
         }
-        for(int i=1;i<R;i++){
-            for(int j=1;j<C;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
+        for(int i=1;i<mat.length;i++){
+            for(int j=1;j<mat[0].length;j++){
+                if(mat[i][0]==0 || mat[0][j]==0){
+                    mat[i][j]=0;
                 }
             }
         }
-        if(matrix[0][0]==0){
-            for(int j=0;j<C;j++){
-                matrix[0][j]=0;
+        if(isFirstRow){
+            for(int i=0;i<mat[0].length;i++){
+                mat[0][i]=0;
             }
         }
-        if(isCol){
-            for(int i=0;i<R;i++){
-                matrix[i][0]=0;
+        if(isFirstCol){
+            for(int i=0;i<mat.length;i++){
+                mat[i][0]=0;
             }
         }
     }
