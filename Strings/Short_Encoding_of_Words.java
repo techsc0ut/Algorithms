84d45054261788e6b3e253fa5e45f91385b248a2
TECH SCOUT <techsc0utOP@gmail.com>
2022-06-20 15:04:54 +0530

    Create Short_Encoding_of_Words.java

---
+++ Strings/Short_Encoding_of_Words.java
@@ -0,0 +1,12 @@
+/* O(N) Time and O(N) Space */
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Arrays.sort(words,(a,b)->(b.length()-a.length()));
+        StringBuilder Sb=new StringBuilder();
+        for(String s:words){
+            if(Sb.indexOf(s+"#")==-1){
+                Sb.append(s+"#");
+            }
+        }return Sb.length();
+    }
+}
