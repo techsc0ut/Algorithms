69a928d21cf84e928b2454b54973b5d2abb2395c
TECH SCOUT <techsc0utOP@gmail.com>
2022-06-20 15:16:03 +0530

    Update Short_Encoding_of_Words.java

--- Strings/Short_Encoding_of_Words.java
+++ Strings/Short_Encoding_of_Words.java
@@ -10,3 +10,19 @@ class Solution {
         }return Sb.length();
     }
 }
+
+/* O(N^2) Time and O(N) Space */
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> St=new HashSet<>(Arrays.asList(words));
+        for(String s:words){
+            for(int i=1;i<s.length();i++){
+                St.remove(s.substring(i));
+            }
+        }
+        int count=0;
+        for(String s:St){
+            count+=s.length()+1;
+        }return count;
+    }
+}
