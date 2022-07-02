9abff326ff18d2d0bfba71e45be1babd98a8c8e4
TECH SCOUT <techsc0utOP@gmail.com>
2022-07-02 12:23:27 +0530

    Update Left_View_Of_Binary_Tree.java

--- Binary_Trees/Left_View_Of_Binary_Tree.java
+++ Binary_Trees/Left_View_Of_Binary_Tree.java
@@ -1,36 +1,22 @@
 class Tree{
     ArrayList<Integer> leftView(Node root){
-        TreeMap<Integer,Integer> T=new TreeMap<>();
-        Queue<temp> Q=new LinkedList<>();
-        Q.add(new temp(root,0));
-        while(!Q.isEmpty()){
-            temp t=Q.poll();
-            Node n=t.n;
-            int h=t.h;
-            Integer a=T.get(h);
-            if(n!=null && a==null){
-                a=n.data;
-                T.put(h,a);
-            }
-            if(n!=null && n.left!=null){
-                Q.offer(new temp(n.left,h+1));
-            }
-            if(n!=null && n.right!=null){
-                Q.offer(new temp(n.right,h+1));
-            }
-        }
         ArrayList<Integer> Res=new ArrayList<>();
-        for(Integer x:T.values()){
-            Res.add(x);
-        }
-        return Res;
-    }
-}
-class temp{
-    Node n;
-    int h;
-    temp(Node _n,int _h){
-        this.n=_n;
-        this.h=_h;
+        Queue<Node> Q=new LinkedList<>();
+        Q.offer(root);
+        while(!Q.isEmpty()){
+            int sz=Q.size();
+            for(int i=0;i<sz;i++){
+                Node tmp=Q.poll();
+                if(i==0 && tmp!=null){
+                    Res.add(tmp.data);
+                }
+                if(tmp!=null && tmp.left!=null){
+                    Q.offer(tmp.left);
+                }
+                if(tmp!=null && tmp.right!=null){
+                    Q.offer(tmp.right);
+                }
+            }
+        }return Res;
     }
 }
