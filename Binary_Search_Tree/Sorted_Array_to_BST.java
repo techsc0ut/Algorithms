20f051a01a468c4dbcbceed8f2adf67de02e443d
TECH SCOUT <techsc0utOP@gmail.com>
2022-07-22 15:56:50 +0530

    Create Sorted_Array_to_BST.java

---
+++ Binary_Search_Tree/Sorted_Array_to_BST.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);   
+    }
+    public TreeNode helper(int[] nums, int low, int high){
+        if(low>high) return null;
+
+        int mid=(low+high)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        
+        root.left=helper(nums,low,mid-1);
+        root.right=helper(nums,mid+1,high);
+        
+        return root;
+    }
+}
