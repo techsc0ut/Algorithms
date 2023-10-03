22656a9126796c03d84ec6231d98081210881686
Rahul Khatoliya <techsc0utOP@gmail.com>
2023-10-04 01:31:40 +0530

    more clean clear code

--- Graphs/Rat_in_a_maze_problem.java
+++ Graphs/Rat_in_a_maze_problem.java
@@ -1,35 +1,70 @@
 class Solution {
-    public static ArrayList<String> findPath(int[][] m, int n) {
-        ArrayList<String> A=new ArrayList<>();
-        if(m[0][0]!=0){
-            int vis[][]=new int[n][n];
-            func(n,A,"",0,0,m,vis);
-        }return A;
+                    // Up,   right,  down,    left
+    int dir [][] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    
+    ArrayList<String> res;
+    
+    public ArrayList<String> findPath(int[][] m, int n) {
+        
+        res = new ArrayList<>();
+        
+        if(m[0][0] == 0) return res;
+        
+        boolean vis[][] = new boolean[n][n];
+        
+        vis[0][0] = true;
+        
+        helper(0, 0, "", m, vis, n);
+        
+        Collections.sort(res);
+        
+        return res;
+        
     }
-    static void func(int n,ArrayList<String> A, String s,int i,int j,int[][]m,int [][]vis){
-        if(i==n-1 && j==n-1){
-            A.add(s);
+    
+    public void helper(int i, int j, String curr, int[][] m, boolean vis[][], int n){
+        
+        if(i == n - 1 && j == n - 1) {
+            
+            res.add(curr);
             return;
+            
         }
-        if(i+1<n && vis[i+1][j]==0 && m[i+1][j]==1){
-            vis[i][j]=1;
-            func(n,A,s+"D",i+1,j,m,vis);
-            vis[i][j]=0;
+        
+        for(int k = 0; k < dir.length; k++){
+            
+            int row = i + dir[k][0];
+            
+            int col = j + dir[k][1];
+            
+            if(row < n && row >= 0 && col < n && col >= 0 && m[row][col] != 0 && !vis[row][col]){
+                
+                vis[row][col] = true;
+                
+                if(k == 0){
+                    
+                    helper(row, col, curr+'U', m, vis, n);
+                    
+                }else if(k == 1){
+                    
+                    helper(row, col, curr+'R', m, vis, n);
+                    
+                }else if(k == 2){
+                    
+                    helper(row, col, curr+'D', m, vis, n);
+                    
+                }else{
+                    
+                    helper(row, col, curr+'L', m, vis, n);
+                    
+                }
+                
+                
+                vis[row][col] = false;
+                
+            }
+            
         }
-        if(j-1>=0 && vis[i][j-1]==0 && m[i][j-1]==1){
-            vis[i][j]=1;
-            func(n,A,s+"L",i,j-1,m,vis);
-            vis[i][j]=0;
-        }
-        if(j+1<n && vis[i][j+1]==0 && m[i][j+1]==1){
-            vis[i][j]=1;
-            func(n,A,s+"R",i,j+1,m,vis);
-            vis[i][j]=0;
-        }
-        if(i-1>=0 && vis[i-1][j]==0 && m[i-1][j]==1){
-            vis[i][j]=1;
-            func(n,A,s+"U",i-1,j,m,vis);
-            vis[i][j]=0;
-        }
+        
     }
 }
