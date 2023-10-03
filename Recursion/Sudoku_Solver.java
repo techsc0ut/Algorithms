20941030ff8d829668f84d5fa3171f814cad543c
Rahul Khatoliya <techsc0utOP@gmail.com>
2023-10-03 23:22:03 +0530

    more clear

--- Recursion/Sudoku_Solver.java
+++ Recursion/Sudoku_Solver.java
@@ -1,63 +1,68 @@
 class Solution {
+
     public void solveSudoku(char[][] board) {
-        char ar[][]=new char[board.length][board[0].length];
-        func(0,0,board,ar);
-        fill(board,ar);
-        return;
+        
+        helper(board, 0, 0);
+
     }
-    static void fill(char ar[][],char board[][]){
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                ar[i][j]=board[i][j];
-            }
+
+    public boolean helper(char[][] board, int i, int j) {
+
+        if (i == board.length) return true;  // Successfully filled the board
+        
+        int ni, nj;
+
+        if (j == board[0].length - 1) {
+
+            ni = i + 1;
+            nj = 0;
+
+        } else {
+
+            ni = i;
+            nj = j + 1;
+
         }
-    }
-    static void func(int i,int j,char[][] board,char ar[][]){
-        if(i==board.length){
-            fill(ar,board); 
-            return ;
-        }
-        int ni=0 ,nj=0;
-        if(j==board[0].length-1){
-            ni=i+1;
-            nj=0;
-        }else{
-            ni=i;
-            nj=j+1;
-        }
-        if(board[i][j]!='.'){
-            func(ni,nj,board,ar);
-        }else{
-            for(int po=1;po<=9;po++){
-                char c=(char)(po+'0'); 
-                if(isValid(i,j,c,board)){
-                    board[i][j]=c;
-                    func(ni,nj,board,ar);
-                    board[i][j]='.';
+
+        if (board[i][j] != '.') {
+
+            return helper(board, ni, nj);  // Move to next cell if current is already filled
+        } else {
+
+            for (char a = '1'; a <= '9'; a++) {
+
+                if (canPut(a, board, i, j)) {
+
+                    board[i][j] = a;
+
+                    if (helper(board, ni, nj)) return true;  // Recurse with the number placed, if solved, return true
+                    board[i][j] = '.';  // Else, backtrack
+
                 }
             }
         }
+        return false;  // No valid number can be placed in this cell
     }
-    static boolean isValid(int i,int j,char c,char[][] board){
-        for(int row=0;row<board.length;row++){
-            if(board[row][j]==c){
-                return false;
+
+    public boolean canPut(char val, char[][] board, int row, int col) {
+
+        for (int c = 0; c < board[0].length; c++) {
+            if (board[row][c] == val) return false;
+        }
+
+        for (int r = 0; r < board.length; r++) {
+            if (board[r][col] == val) return false;
+        }
+
+        int si = 3 * (row / 3);
+        int sj = 3 * (col / 3);
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[si + r][sj + c] == val) return false;
             }
         }
-        for(int col=0;col<board[0].length;col++){
-            if(board[i][col]==c){
-                return false;
-            }
-        }
-        int rr=i/3*3;
-        int cc=j/3*3;
-        for(int a=0;a<3;a++){
-            for(int b=0;b<3;b++){
-                if(board[rr+a][cc+b]==c){
-                    return false;
-                }
-            }
-        }
+        
         return true;
     }
 }
